811b9834090539a47bbfb83d04ec3650bc781dcf
jpoullet2000 <jeanbaptistepoullet@gmail.com>
2018-04-19 16:24:59 +0200

    removing tests

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from atlasclient.client import Atlas
-
-
-@pytest.fixture(scope='module')
-def atlas_client():
-    client = Atlas('localhost', port=21000, username='admin', password='admin')
-    return(client)
